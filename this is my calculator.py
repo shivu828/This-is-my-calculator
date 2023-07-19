8576d1685e60c0e267a2ce6580a703fd0ca6bad8
shivu828 <139810209+shivu828@users.noreply.github.com>
2023-07-19 13:01:41 +0530

    Update this is my calculator.py

--- this is my calculator.py
+++ this is my calculator.py
@@ -1,6 +1,6 @@
-first = ("enter first number : ")
-operator = ("enter operator (+,-,*,/,%) : ")
-second = ("enter second number : ")
+first = input("enter first number : ")
+operator = input("enter operator (+,-,*,/,%) : ")
+second = input("enter second number : ")
 
 first = int(first)
 second = int(second)
